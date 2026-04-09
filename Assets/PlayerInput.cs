663b1d7b211bf8bcf6d2bafc07cde635613a32dc
emirhancanarslan <emrhncnrsln@gmail.com>
2026-04-10 01:49:36 +0300

    player input/movement
    movement with wasd

---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward * Time.deltaTime * 5;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back * Time.deltaTime * 5;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * Time.deltaTime * 5;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * Time.deltaTime * 5;
+        }
+    }
+}
