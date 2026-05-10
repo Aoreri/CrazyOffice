44909be1e6665e81f97d1b8d6f2a4a83e6973ae8
Caner Serbest <36054596+Aoreri@users.noreply.github.com>
2026-05-10 03:21:46 +0300

    Modem Puzzle

--- Assets/Scripts/ModemPuzzle.cs
+++ Assets/Scripts/ModemPuzzle.cs
@@ -1,18 +1,85 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ModemPuzzle : MonoBehaviour
 {
+    public Image lights;
+
     [Header("Antennas")]
     public AntennaRotate anten1;
     public AntennaRotate anten2;
 
+    [Header("UI Transition")]
+    public Image targetImage;
+    public float fadeDuration = 2.0f;
 
-    // Optional: check if both antennas are at a "solved" angle
-    public bool IsSolved(float targetY, float tolerance = 5f)
+    public float acceptableRange = 0.1f;
+
+    private float designatedValue1, designatedValue2;
+
+    void Start()
     {
-        //bool a1ok = Mathf.Abs(anten1 - targetY) <= tolerance;
-        //*bool a2ok = Mathf.Abs(anten2.CurrentRotY - targetY) <= tolerance;
-        return false;
-        //return a1ok && a2ok;
+        anten1.antennaDelta = UnityEngine.Random.value;
+        anten2.antennaDelta = UnityEngine.Random.value;
+
+        anten1.Setup();
+        anten2.Setup();
+
+        designatedValue1 = UnityEngine.Random.value;
+        designatedValue2 = UnityEngine.Random.value;
+    }
+
+    void Update()
+    {
+        float diff1 = Mathf.Abs(anten1.antennaDelta - designatedValue1);
+        float diff2 = Mathf.Abs(anten2.antennaDelta - designatedValue2);
+
+
+        float signal1 = Mathf.InverseLerp(acceptableRange, 0, diff1);
+        float signal2 = Mathf.InverseLerp(acceptableRange, 0, diff2);
+
+        float solveDelta = (signal1 + signal2) / 2f;
+
+        //float diff1 = Mathf.Abs(anten1.antennaDelta - designatedValue1);
+        //float diff2 = Mathf.Abs(anten2.antennaDelta - designatedValue2);
+
+        //float solveDelta = Mathf.Clamp01(1f - (diff1 + diff2) / 2f);
+
+        lights.fillAmount = solveDelta;
+
+        if (solveDelta >= 0.98f)
+        {
+            anten1.enabled = false;
+            anten2.enabled = false;
+            StartCoroutine(FadeImageOut());
+        }
+
+    }
+
+    IEnumerator FadeImageOut()
+    {
+
+        float startAlpha = targetImage.color.a;
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float newAlpha = Mathf.Lerp(startAlpha, 0f, elapsedTime / fadeDuration);
+
+            Color c = targetImage.color;
+            c.a = newAlpha;
+            targetImage.color = c;
+
+            yield return null;
+        }
+
+        Color finalColor = targetImage.color;
+        finalColor.a = 0f;
+        targetImage.color = finalColor;
+
+        yield return new WaitForSeconds(2);
+        gameObject.SetActive(false);
     }
 }
